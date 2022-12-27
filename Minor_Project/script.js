52f12d7b68e86be611f1e68845b2fd1f03f15328
AKASH-KUMAR-12 <akash12082002@gmail.com>
2022-12-27 22:59:17 +0530

    Minor Project@Verzeo

---
+++ Minor_Project/script.js
@@ -0,0 +1,166 @@
+function myOverFunction() {
+    document.querySelector(".o").style.fontSize="80px";
+    document.querySelector(".o").style.color="red";
+    document.querySelector(".od li").style.color="yellow";
+    document.querySelector(".od li").style.fontSize="80px";
+    document.querySelector(".odisha").style.visibility="visible";
+    document.querySelector(".o").style.visibility="visible";
+  }
+  
+  function mouseOut(){
+    document.querySelector(".od li").style.color="red";
+    document.querySelector(".od li").style.fontSize="50px";
+    document.querySelector(".odisha").style.visibility="hidden";
+    document.querySelector(".o").style.visibility="hidden";
+
+  }
+  function myOverFunction2() {
+    document.querySelector(".m").style.fontSize="100px";
+    document.querySelector(".m").style.color="red";
+    document.querySelector(".mad li").style.color="yellow";
+    document.querySelector(".mad li").style.fontSize="58px";
+    document.querySelector(".mp").style.visibility="visible";
+    document.querySelector(".m").style.visibility="visible";
+  }
+  
+  function mouseOut2(){
+   
+    document.querySelector(".mad li").style.color="red";
+    document.querySelector(".mad li").style.fontSize="50px";
+    document.querySelector(".mp").style.visibility="hidden";
+    document.querySelector(".m").style.visibility="hidden";
+    
+  }
+  function myOverFunction3() {
+    document.querySelector(".b").style.fontSize="100px";
+    document.querySelector(".b").style.color="red";
+    document.querySelector(".bi li").style.color="yellow";
+    document.querySelector(".bi li").style.fontSize="80px";
+    document.querySelector(".bihar").style.visibility="visible";
+    document.querySelector(".b").style.visibility="visible";
+  }
+  
+  function mouseOut3(){
+  
+    document.querySelector(".bi li").style.color="red";
+    document.querySelector(".bi li").style.fontSize="50px";
+    document.querySelector(".bihar").style.visibility="hidden";
+    document.querySelector(".b").style.visibility="hidden";
+
+  }
+  function myOverFunction4() {
+    document.querySelector(".ma").style.fontSize="90px";
+    document.querySelector(".ma").style.color="red";
+    document.querySelector(".mah li").style.color="yellow";
+    document.querySelector(".mah li").style.fontSize="70px";
+    document.querySelector(".maha").style.visibility="visible";
+    document.querySelector(".ma").style.visibility="visible";
+  }
+  
+  function mouseOut4(){
+    
+    document.querySelector(".mah li").style.color="red";
+    document.querySelector(".mah li").style.fontSize="50px";
+    document.querySelector(".maha").style.visibility="hidden";
+    document.querySelector(".ma").style.visibility="hidden";
+  }
+  function myOverFunction5() {
+    document.querySelector(".p").style.fontSize="100px";
+    document.querySelector(".p").style.color="red";
+    document.querySelector(".pun li").style.color="yellow";
+    document.querySelector(".pun li").style.fontSize="80px";
+    document.querySelector(".punjab").style.visibility="visible";
+    document.querySelector(".p").style.visibility="visible";
+  }
+  
+  function mouseOut5(){
+   
+    document.querySelector(".pun li").style.color="red";
+    document.querySelector(".pun li").style.fontSize="50px";
+    document.querySelector(".punjab").style.visibility="hidden";
+    document.querySelector(".p").style.visibility="hidden";
+  }
+  function myOverFunction6() {
+    document.querySelector(".r").style.fontSize="100px";
+    document.querySelector(".r").style.color="red";
+    document.querySelector(".raj li").style.color="yellow";
+    document.querySelector(".raj li").style.fontSize="80px";
+    document.querySelector(".rajasthan").style.visibility="visible";
+    document.querySelector(".r").style.visibility="visible";
+  }
+  
+  function mouseOut6(){
+   
+    document.querySelector(".raj li").style.color="red";
+    document.querySelector(".raj li").style.fontSize="50px";
+    document.querySelector(".rajasthan").style.visibility="hidden";
+    document.querySelector(".r").style.visibility="hidden";
+  }
+
+  function myOverFunction7() {
+    document.querySelector(".s").style.fontSize="100px";
+    document.querySelector(".s").style.color="red";
+    document.querySelector(".sik li").style.color="yellow";
+    document.querySelector(".sik li").style.fontSize="80px";
+    document.querySelector(".sikkim").style.visibility="visible";
+    document.querySelector(".s").style.visibility="visible";
+  }
+  
+  function mouseOut7(){
+   
+    document.querySelector(".sik li").style.color="red";
+    document.querySelector(".sik li").style.fontSize="50px";
+    document.querySelector(".sikkim").style.visibility="hidden";
+    document.querySelector(".s").style.visibility="hidden";
+  }
+
+  function myOverFunction8() {
+    document.querySelector(".u").style.fontSize="100px";
+    document.querySelector(".u").style.color="red";
+    document.querySelector(".ut li").style.color="yellow";
+    document.querySelector(".ut li").style.fontSize="66px";
+    document.querySelector(".up").style.visibility="visible";
+    document.querySelector(".u").style.visibility="visible";
+  }
+  
+  function mouseOut8(){
+    
+    document.querySelector(".ut li").style.color="red";
+    document.querySelector(".ut li").style.fontSize="50px";
+    document.querySelector(".up").style.visibility="hidden";
+    document.querySelector(".u").style.visibility="hidden";
+  }
+
+  function myOverFunction9() {
+    document.querySelector(".w").style.fontSize="100px";
+    document.querySelector(".w").style.color="red";
+    document.querySelector(".wes li").style.color="yellow";
+    document.querySelector(".wes li").style.fontSize="74px";
+    document.querySelector(".wb").style.visibility="visible";
+    document.querySelector(".w").style.visibility="visible";
+  }
+  
+  function mouseOut9(){
+    
+    document.querySelector(".wes li").style.color="red";
+    document.querySelector(".wes li").style.fontSize="50px";
+    document.querySelector(".wb").style.visibility="hidden";
+    document.querySelector(".w").style.visibility="hidden";
+  }
+  function myOverFunction10() {
+    document.querySelector(".k").style.fontSize="100px";
+    document.querySelector(".k").style.color="red";
+    document.querySelector(".ker li").style.color="yellow";
+    document.querySelector(".ker li").style.fontSize="80px";
+    document.querySelector(".kerala").style.visibility="visible";
+    document.querySelector(".k").style.visibility="visible";
+  }
+  
+  function mouseOut10(){
+    
+    document.querySelector(".ker li").style.color="red";
+    document.querySelector(".ker li").style.fontSize="50px";
+    document.querySelector(".kerala").style.visibility="hidden";
+    document.querySelector(".k").style.visibility="hidden";
+  }
+  
